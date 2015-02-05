c8c7199bf69770e95ebe9318501c0b50f551155b
LenaTash <Lena.tashkevych@gmail.com>
2015-02-04 20:24:39 -0800

    clean up tutorial parts

--- Tutorial_Part3_models.R
+++ Tutorial_Part3_models.R
@@ -1,31 +1,5 @@
-Installations required:
-R, R packages [h2o, jpeg, pdist, shiny], nConvert
+# Part 3. Building models in H2O
 
-
-### ASSIGN PATHS TO YOUR DIRECTORIES ###
-initial_data_dir <- "~/Documents/ShoeFetish/shoefetish01/shoefetish010/"
-working_data_dir <- "~/Documents/ShoeFetish/shoefetish01/shoefetish010/"
-
-
-### INITIAL DATA TRANSFORMATION ###
-# ADD: read "pre-directory"
-# ADD: convert data in pre-directory to correct format and write into directory
-# ADD: read "directory"
-
-
-### READ IN DATA ###
-library(jpeg)
-x <- readJPEG(directory[1])
-vec <- c(x[,])
-if (length(directory) > 1) {
-    for (i in 2:length(directory)) {
-      x <- readJPEG(directory[i])
-      y <- c(x[,])
-      vec <- cbind(vec, y)
-    }
-}
-vec2 <- as.data.frame(t(vec))
-remove(i,x,vec,y)
 
 predictors <- colnames(vec2)
 
@@ -50,41 +24,5 @@ print(h2o.saveModel(model_main, dir=getwd(), name="model_main", save_cv=TRUE, fo
 ### SAVE TRANSFORMED INITIAL SET ###
 ptrain_main <- as.matrix(h2o.deepfeatures(train_h2o,model_main,layer=3))
 save(ptrain_main, file=paste(getwd(),"/model_main/ptrain_main.RData",sep=""))
-
-
-
-
-### TEST A NEW IMAGE AGAINST PRE-LOADED MODEL ###
-a <- input$newpic[1,]
-listtestfiles <- a$name
-
-system('rm ~/Documents/ShoeFetish/shoefetish01/shoefetish010/testdata/current.jpg')
-system('rm ~/Documents/ShoeFetish/shoefetish01/shoefetish010/testdata/current_small.jpg')
-system(paste('~/Documents/ShoeFetish/shoefetish01/shoefetish010/nconvert -out jpeg -o ~/Documents/ShoeFetish/shoefetish01/shoefetish010/testdata/current.jpg -resize 100 100 -edgedetect light -grey 128 ', a$datapath, sep=""))
-system(paste('~/Documents/ShoeFetish/shoefetish01/shoefetish010/nconvert -out jpeg -o ~/Documents/ShoeFetish/shoefetish01/shoefetish010/testdata/current_small.jpg -resize 100 100 ', a$datapath, sep=""))
-
-library(jpeg)
-x <- readJPEG('~/Documents/ShoeFetish/shoefetish01/shoefetish010/testdata/current.jpg')
-vec <- c(x[,])
-vec3 <- as.data.frame(t(vec))
-remove(x,vec)
-
-test_h2o <- as.h2o(localH2O, vec3, key="test")
-
-ptest <- as.matrix(h2o.deepfeatures(test_h2o,model_main,layer=-1))
-a1 <- pdist(ptest,ptrain_main)
-
-a1_dist <- a1@dist
-a1_n <- a1@n
-a1_p <- a1@p
-b1 <- matrix(a1_dist, nrow=a1_n, ncol=a1_p)
-scores1 <- b1[1,]
-q1 <- as.data.frame(scores1)
-q1$files <- directory
-q1$goodfiles <- directory_show
-q1 <- q1[order(q1$scores),]
-
-c(q1$goodfiles[1],q1$goodfiles[2],q1$goodfiles[3],q1$goodfiles[4],q1$goodfiles[5])
-
 
 
